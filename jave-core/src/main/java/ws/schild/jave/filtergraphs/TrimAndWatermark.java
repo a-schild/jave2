fa6a33058d27bfd7f111328dfdd70dc138aa3788
Michael Ressler <mressler@gmail.com>
2020-08-16 11:32:47 -0400

    Refactoring TrimAndWatermark since the pattern of “do something to a set of videos, then concat and watermark them” is pretty prevalent. This is now switching to a subclass of FilterAndWatermark, which allows for some filter expression to be run on the input videos before handing them off to the concat and watermark portion of the filter.

--- jave-core/src/main/java/ws/schild/jave/filtergraphs/TrimAndWatermark.java
+++ jave-core/src/main/java/ws/schild/jave/filtergraphs/TrimAndWatermark.java
@@ -1,48 +1,27 @@
 package ws.schild.jave.filtergraphs;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-import ws.schild.jave.filters.ConcatFilter;
 import ws.schild.jave.filters.FilterChain;
-import ws.schild.jave.filters.FilterGraph;
-import ws.schild.jave.filters.MovieFilter;
-import ws.schild.jave.filters.OverlayFilter;
 import ws.schild.jave.filters.SetPtsFilter;
 import ws.schild.jave.filters.TrimFilter;
-import ws.schild.jave.filters.helpers.OverlayLocation;
 
 /**
  * Trim and watermark any number of input videos.
  *
- * @author mressler
  */
-public class TrimAndWatermark extends FilterGraph {
-
-  public TrimAndWatermark(File watermark, Double trimStart, Double trimDuration) {
-    super(
-        new FilterChain(
-            new TrimFilter("0", trimStart, trimDuration),
-            new SetPtsFilter().addOutputLabel("main"),
-            new MovieFilter(watermark, "watermark"),
-            new OverlayFilter("main", "watermark", OverlayLocation.BOTTOM_RIGHT, -10, -10)));
-  }
+public class TrimAndWatermark extends FilterAndWatermark {
 
   public TrimAndWatermark(File watermark, List<TrimInfo> trimInfo) {
-    super();
+    super(watermark, trimInfo.size());
 
-    IntStream.range(0, trimInfo.size())
-        .mapToObj(i -> filterchainForInputIndex(i, trimInfo.get(i), watermark))
-        .forEach(this::addChain);
+    init((i, isLast) -> filterchainForTrimInfo(trimInfo.get(i)));
+  }
 
-    addChain(
-        new FilterChain(
-            new ConcatFilter(
-                IntStream.range(0, trimInfo.size())
-                    .mapToObj(this::labelForOutput)
-                    .collect(Collectors.toList()))));
+  public TrimAndWatermark(File watermark, Double trimStart, Double trimDuration) {
+    this(watermark, Arrays.asList(new TrimInfo(trimStart, trimDuration)));
   }
 
   public static class TrimInfo {
@@ -55,16 +34,9 @@ public class TrimAndWatermark extends FilterGraph {
     }
   }
 
-  private FilterChain filterchainForInputIndex(Integer i, TrimInfo info, File watermark) {
+  public static FilterChain filterchainForTrimInfo(TrimInfo info) {
     return new FilterChain(
-        new TrimFilter(i.toString(), info.trimStart, info.trimDuration),
-        new SetPtsFilter().addOutputLabel("trimmed" + i),
-        new MovieFilter(watermark), // Movie output is the second input to the overlay filter
-        new OverlayFilter("trimmed" + i, OverlayLocation.BOTTOM_RIGHT, -10, -10)
-            .addOutputLabel(labelForOutput(i)));
-  }
-
-  private String labelForOutput(Integer i) {
-    return "overlayed" + i;
+        new TrimFilter(info.trimStart, info.trimDuration),
+        new SetPtsFilter());
   }
 }
